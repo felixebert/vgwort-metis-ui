792bd87831c4a84d4b5b360d09b699e62a004a33
Felix Ebert <felix.ebert@if-core.de>
2013-11-18 11:23:50 +0100

    text detail view

--- src/js/text.js
+++ src/js/text.js
@@ -3,12 +3,31 @@
 	var textModule = angular.module('text', []);
 
 	mui.text = {
-		get: function(textId) {
-			return {};
-		}
+		get: function($http, $scope, id) {
+			$http({
+				method: 'GET',
+				url: mui.url('/text/' + id)
+			}).success(function(data) {
+				$scope.text = data;
+			});
+
+			$http({
+				method: 'GET',
+				url: mui.url('/textUrls/' + id)
+			}).success(function(data) {
+				$scope.urls = data;
+			});
+
+			$http({
+				method: 'GET',
+				url: mui.url('/grab/' + id)
+			}).success(function(data) {
+				$scope.grabbedData = data;
+			});
+		},
 	};
 
 	textModule.controller('TextCtrl', function($scope, $http, $routeParams) {
-		$scope.text = mui.text.get($routeParams.id);
+		mui.text.get($http, $scope, $routeParams.id);
 	});
 })(angular, mui);
